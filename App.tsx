64243439ec9a35d152d5478c4fff68638f5608bf
Zeeshan Siddiqui <mzeeshancs8990@gmail.com>
2025-11-02 02:31:13 -0800

    View, Button, Text Components

---
+++ App.tsx
@@ -0,0 +1,18 @@
+import { View, Text,Button } from 'react-native'
+import React from 'react'
+
+
+const App = () => {
+  return (
+
+    <View>
+    <Text style={ { fontSize:30}}>  React Native Mobile Developer </Text>
+    <Text style={ { fontSize:30}}>  Muhammad Zeeshan Siddiqui </Text>
+    <Button title="Press Here"/>
+    </View>
+  );
+    
+  
+}
+
+export default App
