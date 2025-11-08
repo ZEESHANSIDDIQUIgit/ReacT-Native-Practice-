02f75609af38391503d81e252556190ac08fd3cd
Zeeshan_Siddiqui <mzeeshancs8990@gmail.com>
2025-11-08 02:55:53 -0800

    Add files via upload
    Fist Function file

---
+++ src/First.jsx
@@ -0,0 +1,11 @@
+   import React from 'react'
+   import { View, Text} from 'react-native' 
+  const First = () => {
+    return(
+      <View>
+      <Text style={ { fontSize:30}}>  Muhammad Zeeshan Siddiqui </Text>
+      </View>
+    )
+  }
+
+  export default First;
